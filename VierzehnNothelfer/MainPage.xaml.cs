448415b9317a075b6fa671ea79a81b8dbecf08c8
marinakomarek <robert.komarek@gmx.de>
2021-10-09 00:29:03 +0200

    Initial Commit

---
+++ VierzehnNothelfer/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VierzehnNothelfer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await StCatarinaImage.TranslateTo(0, 200, 500);
+            await label.TranslateTo(0, -200, 500);
+            await frame.TranslateTo(0, -200, 500);
+        }
+    }
+}
