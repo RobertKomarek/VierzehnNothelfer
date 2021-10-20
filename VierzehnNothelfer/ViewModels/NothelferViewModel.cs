3e50920b9f8823dc5fff34c1be0bd39246e8e45a
marinakomarek <robert.komarek@gmx.de>
2021-10-20 12:15:23 +0200

    update 2010-1215

--- VierzehnNothelfer/ViewModels/NothelferViewModel.cs
+++ VierzehnNothelfer/ViewModels/NothelferViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
 using VierzehnNothelferBackup.Models;
 using Xamarin.Forms;
 
@@ -7,19 +12,33 @@ namespace VierzehnNothelfer.ViewModels
 {
     public class NothelferViewModel : ContentPage
     {
-        public ObservableCollection<NothelferBackup> VierzehnNothelfer { get; set; }
-        public NothelferBackup MeineNothelfer { get; set; }
-
-        #region COMMANDS
-        #endregion
-
+        public ObservableCollection<NothelferBackup> NothelferCollection { get; set; }
+        //public ObservableCollection<NothelferBackup> VierzehnNothelfer { get; set; }
+        //public NothelferBackup MeineNothelfer { get; set; }
 
         public NothelferViewModel()
         {
-            MeineNothelfer = new NothelferBackup();
-            VierzehnNothelfer = new ObservableCollection<NothelferBackup>(MeineNothelfer.NothelferBackupListe());
-        }
+            var interimList = new List<NothelferBackup>();
 
-      
+            var assembly = typeof(NothelferViewModel).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("VierzehnNothelfer.Resources.14Nothelfer.json");
+
+            using (var reader = new StreamReader(stream))
+            {
+                if (reader != null)
+                {
+                    var jsonString = reader.ReadToEnd();
+                    interimList = JsonConvert.DeserializeObject<List<NothelferBackup>>(jsonString);
+                }
+            }
+
+            var distintHeilige = interimList.GroupBy(x => x.Heiliger).Select(x => x.First()).ToList();
+            NothelferCollection = new ObservableCollection<NothelferBackup>(distintHeilige);
+
+            
+        }
+           
+            //MeineNothelfer = new NothelferBackup();
+            //VierzehnNothelfer = new ObservableCollection<NothelferBackup>(MeineNothelfer.NothelferBackupListe());
     }
 }
